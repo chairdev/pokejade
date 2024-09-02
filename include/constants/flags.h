7ab7f83cc2b61e71a8c81a1f786754441221af3e
Taireru Benaru <tairerub@gmail.com>
2024-09-02 21:53:51 +0200

    starridge gym npcs

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -101,7 +101,7 @@
 #define FLAG_UNUSED_0x055                    0x55  // Unused Flag
 
 #define FLAG_HIDE_CONTEST_POKE_BALL          0x56  // Always set after new game, object it hides is added directly
-#define FLAG_RECEIVED_SODA_POP_BAYCREST_GYM 0x57
+#define FLAG_RECEIVED_SODA_POP_BAYCREST_GYM  0x57
 #define FLAG_BIRCH_AIDE_MET                  0x58
 #define FLAG_DECLINED_BIKE                   0x59
 #define FLAG_RECEIVED_BIKE                   0x5A
@@ -160,7 +160,7 @@
 #define FLAG_DEFEATED_EVIL_TEAM_MT_CHIMNEY   0x8B
 #define FLAG_RECEIVED_6_SODA_POP             0x8C
 #define FLAG_DEFEATED_SEASHORE_HOUSE         0x8D
-#define FLAG_DEVON_GOODS_STOLEN              0x8E
+#define FLAG_RECEIVED_SODA_POP_STARRIDGE_GYM 0x8E
 #define FLAG_RECOVERED_DEVON_GOODS           0x8F
 #define FLAG_RETURNED_DEVON_GOODS            0x90
 #define FLAG_CAUGHT_LUGIA                    0x91
