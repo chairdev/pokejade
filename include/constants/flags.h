38cd4894e3d8d26d539449628db827f94fab3a88
Taireru Benaru <tairerub@gmail.com>
2024-11-11 21:08:12 +0100

    Add new event in Route306 and StarridgeCity maps

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -254,7 +254,7 @@
 #define FLAG_RECEIVED_TM_UNUSED              0xE7
 #define FLAG_RECEIVED_TM_GIGA_DRAIN          0xE8
 
-#define FLAG_UNUSED_0x0E9                    0xE9 // Unused Flag
+#define FLAG_RECEIVED_DUSK_BALL              0xE9 // Unused Flag
 
 #define FLAG_RECEIVED_TM_REST                0xEA
 #define FLAG_RECEIVED_TM_ATTRACT             0xEB
